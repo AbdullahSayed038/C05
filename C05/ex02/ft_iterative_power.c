c288bdc0354260489e741f3d019d2fcf0cc693a2
AbdullahSayed038 <abdullahsayed038@outlook.com>
2024-05-09 01:45:05 +0400

    Add files via upload

---
+++ C05/ex02/ft_iterative_power.c
@@ -0,0 +1,21 @@
+
+int ft_recursive_power(int nb, int power)
+{
+	int res;
+
+	res = nb;
+	if (power < 0)
+	{
+		return (0);
+	}
+	if (power == 0)
+	{
+		return (1);
+	}
+	while (power > 0)
+	{
+		res = res * nb;
+		power--;
+	}
+	return (res);
+}
